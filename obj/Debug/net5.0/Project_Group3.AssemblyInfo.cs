1ce42b2828b25efead83a578290009233f0679e8
levohaihuy <levohaihuy@gmail.com>
2024-02-24 00:05:12 +0700

    Committing changes before switching branches

--- obj/Debug/net5.0/Project_Group3.AssemblyInfo.cs
+++ obj/Debug/net5.0/Project_Group3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e26117d8da157f314d518beccce2c253f5cc1e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fe6dc88f7c82ec937e41c172660f4291c910c35")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
