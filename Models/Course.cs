6d00ad559bb10ec4623c25307737715e6d17b66e
levohaihuy <levohaihuy@gmail.com>
2024-02-23 23:42:12 +0700

    Thêm course va Catetory

---
+++ Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Project_Group3.Models
+{
+    public partial class Course
+    {
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string Description { get; set; }
+        public string CategoryId { get; set; }
+        public string Picture { get; set; }
+        public int? TotalTime { get; set; }
+        public decimal? Price { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Status { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
--- obj/Debug/net5.0/Project_Group3.AssemblyInfo.cs
+++ obj/Debug/net5.0/Project_Group3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e26117d8da157f314d518beccce2c253f5cc1e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
