35037e177e28b7ee23913f678094b449c3bc8513
levohaihuy <levohaihuy@gmail.com>
2024-02-26 00:00:49 +0700

    xoa

--- obj/Debug/net5.0/Project_Group3.AssemblyInfo.cs
+++ obj/Debug/net5.0/Project_Group3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f57633e98e39831feeb2991f59fdef79acb797eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af0a4599a043fdb292793e039e7c584051943ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Group3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
